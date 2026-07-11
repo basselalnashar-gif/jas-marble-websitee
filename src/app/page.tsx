91ed6a62a2d7e138fa2f0c878e239c74874fb604
basselalnashar-gif <basselalnashar@gmail.com>
2026-07-11 17:01:13 +0400

    Add craftsmanship video section to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Hero } from "@/components/home/hero";
 import { Stats } from "@/components/home/stats";
 import { TrustPoints } from "@/components/home/trust-points";
+import { CraftsmanshipShowcase } from "@/components/home/craftsmanship-showcase";
 import { ClientLogos } from "@/components/home/client-logos";
 import { ClosingCTA } from "@/components/home/closing-cta";
 
@@ -10,6 +11,7 @@ export default function HomePage() {
       <Hero />
       <Stats />
       <TrustPoints />
+      <CraftsmanshipShowcase />
       <ClientLogos />
       <ClosingCTA />
     </>
